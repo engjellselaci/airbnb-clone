85ce7ca022150b6c891d77742dfc9d78b70e87ea
Engjell <40763@edu.raseko.fi>
2021-01-27 15:49:50 +0200

    search page

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <p>© 2021 Airbnb Clone! No right reserved this is just a demo!</p>
+      <p>© 2021 Airbnb Clone! No right reserved - this is just a demo!</p>
       <p>Privacy · Terms · Sitemap · Company Details</p>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -6,11 +6,12 @@ import "react-date-range/dist/theme/default.css";
 import "date-fns";
 import { Button } from "@material-ui/core";
 import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
 
 function Search() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-
+  const history = useHistory();
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
@@ -29,7 +30,7 @@ function Search() {
         <PeopleIcon />{" "}
       </h2>
       <input min={0} defaultValue={2} type="number" />
-      <Button>Search Airbnb</Button>
+      <Button onClick={() => history.push("/search")}>Search Airbnb</Button>
     </div>
   );
 }
